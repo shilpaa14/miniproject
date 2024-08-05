de3898fcf2b9edd6d94e658817651dd907e8112f
shilpaa14 <21i29.shilpa@sjec.ac.in>
2024-08-05 17:25:51 +0530

    first commit

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Our Diet and Food Recommendation System! 👋")
+
+
+st.markdown(
+    """
+    Get Started Today!
+
+    Ready to discover meals that are not only delicious but also aligned with your dietary goals? Start now and let our Diet and Custom Food Recommendation System guide you to a healthier, happier you!
+    """
+)
